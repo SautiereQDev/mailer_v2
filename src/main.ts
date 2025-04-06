bd44a564d8ba40151cf981ab6fb1db21ffd39032
Quentin Sautiere <contact@quentinsautiere.com>
2025-04-07 00:58:36 +0200

    enhance main.ts to support Handlebars views and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'express-handlebars';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  const hbsInstance = hbs.create({
+    extname: 'hbs',
+    partialsDir: join(__dirname, '..', 'views', 'partials'),
+    layoutsDir: join(__dirname, '..', 'views', 'layouts'),
+    defaultLayout: 'main',
+  });
+
+  app.engine('hbs', hbsInstance.engine);
 
   await app.listen(
     parseInt(configService.get<string>('PORT') ?? '3000', 10),
@@ -17,4 +35,4 @@ async function bootstrap() {
   );
 }
 
-bootstrap();
+void bootstrap();
